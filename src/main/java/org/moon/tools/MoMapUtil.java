f5e63a11501fbe132590ee14945d63a50d666d3e
moncat <moncat@users.noreply.github.com>
2023-08-22 10:56:35 +0800

    新增MomapUtil

---
+++ src/main/java/org/moon/tools/MoMapUtil.java
@@ -0,0 +1,16 @@
+package org.moon.tools;
+
+/**
+ * @Projectname: moonTools
+ * @Filename: MoMapUtil
+ * @Author: zhang yongling
+ * @Data:2023/8/22 10:55
+ * @Description: TODO
+ */
+public class MoMapUtil {
+
+    public  static String  map(String str){
+        return System.nanoTime()+"-------------"+str;
+    }
+
+}
